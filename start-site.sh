1aa5ba7fae0f79a5c425f3549550e9d63323d47c
mariyaraheal1 <mariyaraheal1@gmail.com>
2026-07-25 18:52:27 +0000

    tried to make the port permenent in the start-site.sh file, but it is not working. I will try to fix it later.

---
+++ start-site.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+cd /workspaces/Personal-Site
+if ! pgrep -f "python3 -m http.server 8000" >/dev/null 2>&1; then
+  nohup python3 -m http.server 8000 --bind 0.0.0.0 > /tmp/personal-site-http.log 2>&1 &
+fi
